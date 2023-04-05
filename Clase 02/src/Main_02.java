9da1b1383e25bced5e9de4410ca3b885f3742cbd
sdmmza <71900740+sdmmza@users.noreply.github.com>
2023-04-05 09:42:37 -0300

    Ejercicios de la Clase Nº02

---
+++ Clase 02/src/Main_02.java
@@ -0,0 +1,10 @@
+public class Main_02 {
+    public static void main(String[] args) {
+
+        //--------------------------------------------------------------------------------
+        //CLASE Nº2 - EJERCICIO Nº
+        System.out.println("\n--------------------\nClase Nº2 - Ejercicio Nº\n");
+
+        System.out.println("\n--------------------");
+    }
+}
